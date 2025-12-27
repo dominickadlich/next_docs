fdeb73fcf9caac3cc9cd0a9ccf8a86be8fab1734
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-12-27 09:56:24 -0600

    Added Carousel to ui/dashboard

---
+++ nextjs-dashboard/app/ui/dashboard/carousel.tsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+
+import CardWrapper from "./cards"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export function CarouselDemo() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <CardWrapper>
+                
+              </CardWrapper>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
